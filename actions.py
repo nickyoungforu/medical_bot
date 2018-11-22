f176acf9fe17e3c8f479b5cedc50c3d5f6f95e41
chenzy <167993935@qq.com>
2018-11-22 18:54:59 +0800

    demo 0.1

--- actions.py
+++ actions.py
@@ -5,39 +5,72 @@
 
 
 from rasa_core_sdk import ActionExecutionRejection
-from rasa_core_sdk.forms import FormAction, REQUESTED_SLOT
-from rasa_core_sdk.events import SlotSet, Restarted
+from rasa_core_sdk.forms import FormAction, REQUESTED_SLOT, logger
+from rasa_core_sdk.events import SlotSet, Form
 
 
 class Action_Total(FormAction):
 
     RANDOMIZE = True
-
+    required_slots_flag = -1
+    slots_filled = set()
     def name(self):
         return 'request_form'
 
-    @staticmethod
-    def required_slots(tracker):
+    # @staticmethod
+    # def required_slots_mapping(tracker):
+    #     last_entity = tracker.latest_message.get('entities')
+    #     def validate_entity(entity):
+    #         if entity:
+    #             if set([k_v['entity'] for k_v in entity]).issubset({"department", "admission-time", "bed-number"}):
+    #         pass
+    #     return {
+    #         1: 1
+    #     }
+    #     pass
+    # 直接屏蔽基类method, 不重写
+    def required_slots(self, tracker):
         slots = set([k for k, v in tracker.slots.items()])
-        # a = tracker.latest_message.get('intent')['name']
-        # # return ["patient-name", "admission-time"]
-        # if tracker.get_slot('patient-name') == 'kk':
-        #     return ["patient-name", "admission-time"]
-        # else:
-        #     return ["patient-name", "admission-time"]
-        if tracker.latest_message.get('intent')['name'] == 'request_patient_info':
-            last_entity = tracker.latest_message.get('entities')
-            if last_entity:
-                if set(last_entity[0]['entity']).issubset({"department", "admission-time", "bed-number"}):
-            # if slots.issubset({"department", "admission-time", "bed-number"}):
-                    return ["department", "admission-time", "bed-number"]
-                else:
-                    return ["patient-name", "admission-time"]
-            else:
-                return ["patient-name", "admission-time"]
+        last_entity = tracker.latest_message.get('entities')
+        required_slots_mapping = {
+            0: {'path1': ["department", "admission-time", "bed-number"],
+                'path2': ["patient-name", "admission-time"]},
+            1: {'path1': ["department", "admission-time", "bed-number", "inspection-name"],
+                'path2': ["patient-name", "admission-time", "inspection-name"]},
+            2: {'path1': ["department", "admission-time", "bed-number", "inspection-time"],
+                'path2': ["patient-name", "admission-time", "inspection-time"]},
+            3: {'path1': ["department", "admission-time", "bed-number", "laboratory-indicator"],
+                'path2': ["patient-name", "admission-time", "laboratory-indicator"]},
+            4: {'path1': ["department", "admission-time", "bed-number", "order-name"],
+                'path2': ["patient-name", "admission-time", "order-name"]},
+            5: {'path1': ["department", "admission-time", "bed-number", "disease-name"],
+                'path2': ["patient-name", "admission-time", "disease-name"]},
+            6: {'path1': [],
+                'path2': []},
+            7: {'path1': ["department", "admission-time", "bed-number", "laboratory-indicator"],
+                'path2': ["patient-name", "admission-time", "laboratory-indicator"]},
+            -1: {'path1': None,
+                 'path2': None},
+        }
+        # if self.slots_filled:
+        if self.slots_filled.issubset({"department", "admission-time", "bed-number"}) and self.slots_filled:
+            return required_slots_mapping[self.required_slots_flag]['path1']
         else:
-            return ["patient-name", "admission-time"]
-        # return ["patient-name", "admission-time"]
+            return required_slots_mapping[self.required_slots_flag]['path2']
+
+
+    def request_next_slot(self,
+                          dispatcher,
+                          tracker,
+                          domain
+                          ):
+        for slot in self.required_slots(tracker):
+            if self._should_request_slot(tracker, slot):
+                dispatcher.utter_template("utter_ask_{}".format(slot), tracker)
+                return [SlotSet(REQUESTED_SLOT, slot)]
+        return None
+
+
 
     def slot_mappings(self):
         # return {"patient-name": self.from_entity(entity="patient-name",),
@@ -66,6 +99,7 @@ class Action_Total(FormAction):
         """
         # extract other slots that were not requested
         # but set by corresponding entity
+        self.slots_filled.update([entities['entity'] for entities in tracker.latest_message.get('entities')])
         slot_values = self.extract_other_slots(dispatcher, tracker, domain)
 
         # extract requested slot
@@ -95,6 +129,7 @@ class Action_Total(FormAction):
             elif slot == 'sth...':
                 pass
         '''
+
         # validation succeed, set the slots values to the extracted values
         return [SlotSet(slot, value) for slot, value in slot_values.items()]
 
@@ -108,3 +143,41 @@ class Action_Total(FormAction):
         # return [Restarted()]
         # 若在此restart response的event执行后，预测后续action时,tracker已空
         return []
+
+    def _activate_if_required(self, tracker):
+        if tracker.active_form.get('name') is not None:
+            logger.debug("The form '{}' is active"
+                         "".format(tracker.active_form))
+        else:
+            logger.debug("There is no active form")
+
+        if tracker.active_form.get('name') == self.name():
+            return []
+        else:
+            if tracker.latest_message.get('intent')['name'] in ['request_patient_info', 'request_patient_emr', 'request_patient_detail']:
+                self.required_slots_flag = 0
+            elif tracker.latest_message.get('intent')['name'] == 'request_patient_inspection_report':
+                self.required_slots_flag = 1
+            elif tracker.latest_message.get('intent')['name'] == 'request_patient_laboratory_report':
+                self.required_slots_flag = 2
+            elif tracker.latest_message.get('intent')['name'] == 'request_patient_abnormal':
+                self.required_slots_flag = 3
+            elif tracker.latest_message.get('intent')['name'] == 'request_patient_Medical_order':
+                self.required_slots_flag = 4
+            elif tracker.latest_message.get('intent')['name'] == ['request_similar_patient', 'request_related_literature', 'request_related_guide']:
+                self.required_slots_flag = 5
+            elif tracker.latest_message.get('intent')['name'] == 'request_collect':
+                self.required_slots_flag = 6
+            elif tracker.latest_message.get('intent')['name'] == 'request_patient_laboratory_indicator':
+                self.required_slots_flag = 7
+            else:
+                self.required_slots_flag = -1
+            logger.debug("Activated the form '{}'".format(self.name()))
+            return [Form(self.name())]
+
+    def _deactivate(self):
+        self.required_slots_flag = -1
+        self.slots_filled.clear()
+        logger.debug("Deactivating the form '{}'".format(self.name()))
+        return [Form(None), SlotSet(REQUESTED_SLOT, None)]
+
--- data/stories.md
+++ data/stories.md
@@ -6,3 +6,82 @@
     - utter_slots_values
     - action_restart
 
+## 2
+* request_patient_emr
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
+
+## 3
+* request_patient_inspection_report
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
+
+## 4
+* request_patient_laboratory_report
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
+
+## 5
+* request_patient_laboratory_indicator
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
+
+## 6
+* request_patient_abnormal
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
+
+## 7
+* request_patient_Medical_order
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
+
+## 8
+* request_similar_patient
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
+
+## 9
+* request_related_literature
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
+
+## 10
+* request_related_guide
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
+
+## 11
+* request_collect
+    - request_form
+    - form{"name": "request_form"}
+    - form{"name": null}
+    - utter_slots_values
+    - action_restart
