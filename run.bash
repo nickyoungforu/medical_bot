e7585f5e7cf29f7a571fb5b8dfd0bb408ee98e9e
chenzy <167993935@qq.com>
2018-12-04 14:57:43 +0800

    rm redundant files

--- run.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python -m rasa_core.run --core models/dialogue --nlu models/nlu/current --debug --endpoints endpoint.yml --connector rest --port 5002 --credentials credentials.yml
--- train_nlu.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python -m rasa_nlu.train -c nlu_config.yml --fixed_model_name current --data data/nlu.json -o models --project nlu --verbose
